43863f72e6b8ec947617bdc1c67c230f76a47de2
가동재 <masterforce999@naver.com>
2024-11-06 17:28:51 +0900

    git init

--- wp-start/src/index.js
+++ wp-start/src/index.js
@@ -5,6 +5,8 @@ function component() {
 
   /* lodash is required for the next line to work */
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  //굳이 로대시를 쓴 이유는? 로대시 그냥 문자열 넣은 거 보여줄려소
+  //https://lodash.com/docs/4.17.15#join 한번 읽어봐
 
   return element;
 }
--- wp-start/webpack.config.js
+++ wp-start/webpack.config.js
@@ -1,12 +1,16 @@
 // webpack.config.js
 // `webpack` command will pick up this config setup by default
 var path = require("path");
+//node.js common.js 모둘문법 path라는 라이브러리를 사용하여 변수에 담았다.
 
 module.exports = {
   mode: "none",
-  entry: "./src/index.js",
+  entry: "./src/index.js", // 이경로를 돌려서
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-  },
+    filename: "main.js", // 이 이름으로
+    path: path.resolve(__dirname, "dist"), //path라는 라이브러리에 해당 api를 사용
+  }, // 이 결과값을 리턴한다. path에 생성
 };
+
+//Node.js Path API 문서 
+//https://nodejs.org/api/path.html 읽어봅시다
